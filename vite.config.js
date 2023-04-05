f229f712cd73c7f39813c325997822529475f5b7
anyell <122279317+anyellpenascal@users.noreply.github.com>
2023-04-05 09:16:07 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+      rollupOptions: {
+        external: [
+          /^src\/assets\/iconos\/.*\.png$/,
+          /^src\/assets\/image\/.*\.png$/,
+          /^src\/assets\/avatares\/.*\.png$/,
+          /^src\/assets\/Avatares\/.*\.svg$/,
+          'src/assets/Avatares/moha.png'
+        ],
+      },
+    },
   plugins: [vue()],
   resolve: {
     alias: {
